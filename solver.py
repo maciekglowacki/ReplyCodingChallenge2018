32a50e75edc1e628b62dde6dd5aca79f7cfcfe9d
ArturMarcinkowski <takiotaku@gmail.com>
2020-03-11 22:55:00 +0100

    added availability index counter

--- solver.py
+++ solver.py
@@ -8,6 +8,18 @@ def average_latency(project, regions, packages_count):
         print(project.country)
         services_count += (sum(region.package_info.services_per_package.values()) * package_count)
 
+def availability_index(project, regions, packages_count):
+    services_count = []
+    for region, package_count in zip(regions,packages_count):
+        services_count.append(sum(region.package_info.services_per_package.values()) * package_count)
+    
+    q1 = sum(services_count)**2
+    q2 = 0
+    for x in services_count
+        q2 += x.services_count**2
+    availability_index_value = q1/q2
+        
+
 
 
 
